fa578563919c331cd5476bcc24c7f866310e3d0b
Mateusz <zubermateusz@gmail.com>
2023-01-13 09:38:14 +0100

    Stage 1/4: Rush into print

--- Zookeeper/task/src/Main.java
+++ Zookeeper/task/src/Main.java
@@ -1,5 +1,26 @@
+/*
+* To begin with, you will develop a simple printer. Your program should display the text from the output example.
+
+Example
+The output:
+
+I love animals!
+Let's check on the animals...
+The deer looks fine.
+The bat looks happy.
+The lion looks healthy.
+*
+*
+* */
+
 public class Main {
     public static void main(String[] args) {
         // write your code here
+        System.out.println("""
+                I love animals!
+                Let's check on the animals...
+                The deer looks fine.
+                The bat looks happy.
+                The lion looks healthy.""");
     }
 }
