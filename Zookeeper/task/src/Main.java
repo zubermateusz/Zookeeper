f42abfba3dbcc0d987487078050ccc21e4171bac
Mateusz <zubermateusz@gmail.com>
2023-01-13 09:55:33 +0100

    Stage 2/4: Show me an animal!

--- Zookeeper/task/src/Main.java
+++ Zookeeper/task/src/Main.java
@@ -1,26 +1,56 @@
 /*
-* To begin with, you will develop a simple printer. Your program should display the text from the output example.
+* One of the most important parts of working with animals is keeping an eye on them. We need to see the animals on the screen to know how they are doing, right? Now we are ready to print something awesome: an image of an animal!
+
+Objectives
+For the second stage, you will need to develop an animal printer. Your program should display the animal identified in the code field.
 
 Example
-The output:
+Your output should contain the following ASCII image:
 
-I love animals!
-Let's check on the animals...
-The deer looks fine.
-The bat looks happy.
-The lion looks healthy.
-*
-*
+Switching on the camera in the camel habitat...
+ ___.-''''-.
+/___  @    |
+',,,,.     |         _.'''''''._
+     '     |        /           \
+     |     \    _.-'             \
+     |      '.-'                  '-.
+     |                               ',
+     |                                '',
+      ',,-,                           ':;
+           ',,| ;,,                 ,' ;;
+              ! ; !'',,,',',,,,'!  ;   ;:
+             : ;  ! !       ! ! ;  ;   :;
+             ; ;   ! !      ! !  ; ;   ;,
+            ; ;    ! !     ! !   ; ;
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+Look at that! Our little camel is sunbathing!
 * */
-
 public class Main {
     public static void main(String[] args) {
+        String camel = """
+            Switching on the camera in the camel habitat...
+             ___.-''''-.
+            /___  @    |
+            ',,,,.     |         _.'''''''._
+                 '     |        /           \\
+                 |     \\    _.-'             \\
+                 |      '.-'                  '-.
+                 |                               ',
+                 |                                '',
+                  ',,-,                           ':;
+                       ',,| ;,,                 ,' ;;
+                          ! ; !'',,,',',,,,'!  ;   ;:
+                         : ;  ! !       ! ! ;  ;   :;
+                         ; ;   ! !      ! !  ; ;   ;,
+                        ; ;    ! !     ! !   ; ;
+                        ; ;    ! !    ! !     ; ;
+                       ;,,      !,!   !,!     ;,;
+                       /_I      L_I   L_I     /_I
+            Look at that! Our little camel is sunbathing!""";
+
         // write your code here
-        System.out.println("""
-                I love animals!
-                Let's check on the animals...
-                The deer looks fine.
-                The bat looks happy.
-                The lion looks healthy.""");
+        System.out.println(camel);
     }
 }
