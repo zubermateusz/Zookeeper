a8be713462af4d7fda00cf71c04ccee5e659bb5c
Mateusz <zubermateusz@gmail.com>
2023-01-13 10:11:39 +0100

    Stage 3/4: What's inside?

--- Zookeeper/task/src/Main.java
+++ Zookeeper/task/src/Main.java
@@ -1,32 +1,27 @@
+
+
 /*
-* One of the most important parts of working with animals is keeping an eye on them. We need to see the animals on the screen to know how they are doing, right? Now we are ready to print something awesome: an image of an animal!
+* The third stage requires you to increase the capabilities of your software. Now it should be able to recognize the number of a specific habitat from the input and show the animals living there.
+
+Add all of the variables from the template to a single variable with the list type. The order of variables matters: they must appear on the list in the order in which they're defined in the code. The list must contain all of the variables with no duplicates.
 
 Objectives
-For the second stage, you will need to develop an animal printer. Your program should display the animal identified in the code field.
+In this stage your program should:
 
-Example
-Your output should contain the following ASCII image:
+Ask for the number of the desired habitat using the following phrase: Please enter the number of the habitat you would like to view:
 
-Switching on the camera in the camel habitat...
- ___.-''''-.
-/___  @    |
-',,,,.     |         _.'''''''._
-     '     |        /           \
-     |     \    _.-'             \
-     |      '.-'                  '-.
-     |                               ',
-     |                                '',
-      ',,-,                           ':;
-           ',,| ;,,                 ,' ;;
-              ! ; !'',,,',',,,,'!  ;   ;:
-             : ;  ! !       ! ! ;  ;   :;
-             ; ;   ! !      ! !  ; ;   ;,
-            ; ;    ! !     ! !   ; ;
-            ; ;    ! !    ! !     ; ;
-           ;,,      !,!   !,!     ;,;
-           /_I      L_I   L_I     /_I
-Look at that! Our little camel is sunbathing!
-* */
+Use the input number as an index of your habitats to print its content.
+
+End with the following phrase:
+
+---
+You've reached the end of the program. To check another habitat, please restart the watcher.
+Examples
+The greater-than symbol followed by a space (> ) represents the user input. Notice that it's not part of the input.*/
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         String camel = """
@@ -50,7 +45,122 @@ public class Main {
                        /_I      L_I   L_I     /_I
             Look at that! Our little camel is sunbathing!""";
 
+        String lion = """
+            Switching on the camera in the lion habitat...
+                                                           ,w.
+                                                         ,YWMMw  ,M  ,
+                                    _.---.._   __..---._.'MMMMMw,wMWmW,
+                               _.-""        '''           YP"WMMMMMMMMMb,
+                            .-' __.'                   .'     MMMMW^WMMMM;
+                _,        .'.-'"; `,       /`     .--""      :MMM[==MWMW^;
+             ,mM^"     ,-'.'   /   ;      ;      /   ,       MMMMb_wMW"  @\\
+            ,MM:.    .'.-'   .'     ;     `\\    ;     `,     MMMMMMMW `"=./`-,
+            WMMm__,-'.'     /      _.\\      F'''-+,,   ;_,_.dMMMMMMMM[,_ / `=_}
+            "^MP__.-'    ,-' _.--""   `-,   ;       \\  ; ;MMMMMMMMMMW^``; __|
+                       /   .'            ; ;         )  )`{  \\ `"^W^`,   \\  :
+                      /  .'             /  (       .'  /     Ww._     `.  `"
+                     /  Y,              `,  `-,=,_{   ;      MMMP`""-,  `-._.-,
+                    (--, )                `,_ / `) \\/"")      ^"      `-, -;"\\:
+            The lion is roaring!""";
+
+        String deer = """
+            Switching on the camera in the deer habitat...
+               /|       |\\
+            `__\\       //__'
+               ||      ||
+             \\__`\\     |'__/
+               `_\\   //_'
+               _.,:---;,._
+               \\_:     :_/
+                 |@. .@|
+                 |     |
+                 ,\\.-./ \\
+                 ;;`-'   `---__________-----.-.
+                 ;;;                         \\_\\
+                 ';;;                         |
+                  ;    |                      ;
+                   \\   \\     \\        |      /
+                    \\_, \\    /        \\     |\\
+                      |';|  |,,,,,,,,/ \\    \\ \\_
+                      |  |  |           \\   /   |
+                      \\  \\  |           |  / \\  |
+                       | || |           | |   | |
+                       | || |           | |   | |
+                       | || |           | |   | |
+                       |_||_|           |_|   |_|
+                      /_//_/           /_/   /_/
+            Our 'Bambi' looks hungry. Let's go to feed it!""";
+
+        String goose = """
+            Switching on the camera in the goose habitat...
+            
+                                                _
+                                            ,-"" "".
+                                          ,'  ____  `.
+                                        ,'  ,'    `.  `._
+               (`.         _..--.._   ,'  ,'        \\    \\
+              (`-.\\    .-""        ""'   /          (  d _b
+             (`._  `-"" ,._             (            `-(   \\
+             <_  `     (  <`<            \\              `-._\\
+              <`-       (__< <           :
+               (__        (_<_<          ;
+                `------------------------------------------
+            The goose is staring intently at you... Maybe it's time to change the channel?""";
+
+        String bat = """
+            Switching on the camera in the bat habitat...
+            _________________               _________________
+             ~-.              \\  |\\___/|  /              .-~
+                 ~-.           \\ / o o \\ /           .-~
+                    >           \\  W  //           <
+                   /             /~---~\\             \\
+                  /_            |       |            _\\
+                     ~-.        |       |        .-~
+                        ;        \\     /        i
+                       /___      /\\   /\\      ___\\
+                            ~-. /  \\_/  \\ .-~
+                               V         V
+            This bat looks like it's doing fine.""";
+
+        String rabbit = """
+            Switching on the camera in the rabbit habitat...
+                     ,
+                    /|      __
+                   / |   ,-~ /
+                  Y :|  //  /
+                  | jj /( .^
+                  >-"~"-v"
+                 /       Y
+                jo  o    |
+               ( ~T~     j
+                >._-' _./
+               /   "~"  |
+              Y     _,  |
+             /| ;-"~ _  l
+            / l/ ,-"~    \\
+            \\//\\/      .- \\
+             Y        /    Y
+             l       I     !
+             ]\\      _\\    /"\\
+            (" ~----( ~   Y.  )
+            It looks like we will soon have more rabbits!""";
+
         // write your code here
-        System.out.println(camel);
+
+        ArrayList<String> listOfAnimal = new ArrayList<>();
+        listOfAnimal.add(camel);
+        listOfAnimal.add(lion);
+        listOfAnimal.add(deer);
+        listOfAnimal.add(goose);
+        listOfAnimal.add(bat);
+        listOfAnimal.add(rabbit);
+
+        System.out.print("Please enter the number of the habitat you would like to view:");
+        Scanner scanner = new Scanner(System.in);
+        int userChoose = scanner.nextInt();
+        System.out.println(listOfAnimal.get(userChoose));
+
+        System.out.println("---");
+        System.out.println("You've reached the end of the program. To check another habitat, please restart the watcher.");
     }
 }
